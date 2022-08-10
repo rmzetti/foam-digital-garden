f0fada27e7c526ecdf957262152a78b139728497
Ralph Marzetti <rmzetti@gmail.com>
2022-08-10 23:33:36 +1200

    update

--- src/gatsby-theme-garden/components/mdx-components/index.js
+++ src/gatsby-theme-garden/components/mdx-components/index.js
@@ -1,2 +1,10 @@
 export { AnchorTag as a } from "gatsby-theme-garden/src/components/mdx-components/anchor-tag"
 export code from "./CodeBlock"
+
+
+// Could not find a declaration file for module 'gatsby-theme-garden/src/components/mdx-components/anchor-tag'. 
+//'C:/Users/ralph/OneDrive/Documents/GitHub/foam-digital-garden/node_modules/gatsby-theme-garden/src/components/mdx-components/anchor-tag.js' 
+//implicitly has an 'any' type.
+//   Try `npm i --save-dev @types/gatsby-theme-garden` if it exists
+//or add a new declaration (.d.ts) file containing 
+//`declare module 'gatsby-theme-garden/src/components/mdx-components/anchor-tag';`
